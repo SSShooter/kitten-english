cdb42000e2525befa5b10e8ddaa5676fffd334cc
ssshooter <ssshooterx@gmail.com>
2025-03-23 20:04:36 +0800

    chore: add Netlify adapter and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,32 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Kitten English',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: '基础',
-					autogenerate: { directory: 'basic' },
-				},
-				{
-					label: '语法专题',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: '进阶',
-					autogenerate: { directory: 'extra' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'Kitten English',
+          social: {
+              github: 'https://github.com/withastro/starlight',
+          },
+          sidebar: [
+              {
+                  label: '基础',
+                  autogenerate: { directory: 'basic' },
+              },
+              {
+                  label: '语法专题',
+                  autogenerate: { directory: 'guides' },
+              },
+              {
+                  label: '进阶',
+                  autogenerate: { directory: 'extra' },
+              },
+          ],
+      }),
 	],
+
+  adapter: netlify(),
 });
